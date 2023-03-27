4d6c77878b39651b8ada278c59c03a82e506665f
dudulu <1791105501@gmail.com>
2023-03-27 08:57:39 +0800

    update：优化vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import viteCompression from 'vite-plugin-compression'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin(), viteCompression()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, 'src'),
       'antd/dist/reset.css': path.join(__dirname, 'node_modules/antd/dist/reset.css'),  // <-- add this
       antd: path.join(__dirname, 'node_modules/antd/dist/antd.js'),
       '@ant-design/icons': path.join(__dirname, 'node_modules/@ant-design/icons/dist/index.umd.js'),
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      cache: true,
+      treeshake: true,
     }
   }
 })
